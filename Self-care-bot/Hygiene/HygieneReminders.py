3a6ab579956b2ea2e84500d90929d7ff6cce4ecc
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-11-04 14:46:04 -0800

    HygieneReminders.py
    Hygienic reminders for the self care bot reminder system.

---
+++ Self-care-bot/Hygiene/HygieneReminders.py
@@ -0,0 +1,17 @@
+# Start of script
+# Self care bot Hygiene reminders
+def bathShower():
+  print ("Remember to take a bath or shower this morning")
+def bathShowerNocturnal():
+  print ("Remember to take a bath or shower tonight")
+def morningBrushing():
+  print ("Remember to brush your teeth for 2 minutes this morning")
+def nighttimeBrushing():
+  print ("Remember to brush your teeth for 2 minutes tonight before bed/work")
+# End of script
+"""
+File info
+File version: 1 (Tuesday, November 4th 2020 at 2:46 pm)
+File type: Python script file (*.py)
+Line count (including blank lines and compiler line): 18
+"""
