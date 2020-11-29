b36966568fa91c2c5402921a4a4e246502c1ba00
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-11-28 18:23:42 -0800

    NegativeMediaDef_V1.py
    An archive of the first version of the negative media definition file.

---
+++ Self-care-bot/Mood/NegativeMedia/OldVersions/NegativeMediaDef_V1.py
@@ -0,0 +1,16 @@
+# Start of script
+def negativeMedia():
+  print("Definition - Negative media | Self care bot")
+  print("The idea to limit negative media is good for mental health.")
+  print("One of the first instances of the activity during the day should be avoidance of negative media posts (on all websites) about yourself or others for the first 1-2 hours of the day. This can make you feel less helpless/hopeless/scared/upset in the morning")
+  print("Also, try to limit your usage of echo chamber sites that enforce negativity for user engagement, such as Facebook, Instagram, Twitter, Reddit, and other known negative mediums.")
+#}
+print (negativeMedia())
+noMore = input("Press [ENTER] key to quit")
+print ("The window should now be closed. If this text is still showing, try pressing the close button. If that doesn't work, end the process/task using a process manager/task manager")
+""" File info
+File version: 1 (Saturday, November 28th 2020 at 6:23 pm)
+File type: Python script file (*.py)
+Line count (including blank lines and compiler line): 17
+"""
+# End of script
